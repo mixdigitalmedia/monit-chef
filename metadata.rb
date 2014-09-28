fcbb51957bbe4dfd66ea78eb2a4d0a24fcb9ea02
Kenton Newby <kentonnewby@gmail.com>
2014-09-27 20:26:00 -0400

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name              "monit-chef"
+maintainer        "Mix Digital Media, LLC"
+maintainer_email  "info@mixdigitalmedia.com"
+description       "Basic monit installation"
+version           "0.0.1"
+
+supports      "ubuntu"
